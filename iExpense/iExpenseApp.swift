a572918358913d413d70acd9aace80c1479cf97c
Ojidaemo <vit.martsinovich@gmail.com>
2023-07-23 14:43:09 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Vitali Martsinovich on 2023-07-23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
